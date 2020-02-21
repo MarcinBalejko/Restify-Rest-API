8fed820f93daddc9f2040d9904179219cdd71420
MarcinBalejko <mbbm9@o2.pl>
2020-02-21 23:46:19 +0100

    :white_check_mark: Established server and connection to mongodb

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const config = require('./config');
 
 const dotenv = require('dotenv');
 dotenv.config();
+
+const server = restify.createServer();
+
+server.use(restify.plugins.bodyParser());
+
+server.listen(config.PORT, () => {
+    mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+    console.log('Connected to MongoDB!');
+});
+
+const db = mongoose.connection;
+

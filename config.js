ee95abacf232de27841b16d78348bf771b443a75
MarcinBalejko <mbbm9@o2.pl>
2020-02-21 23:07:28 +0100

    :white_check_mark: Starter setup

---
+++ config.js
@@ -0,0 +1,9 @@
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+module.exports = {
+    ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 3000,
+    URL: process.env.BASE_URL,
+    MONGODB_URI: process.env.MONGODB_URI
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+const restify = require('restify');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const dotenv = require('dotenv');
+dotenv.config();

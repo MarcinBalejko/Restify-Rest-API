cd99b2d324ee3e216042808a5acb5678aadf260d
MarcinBalejko <mbbm9@o2.pl>
2020-02-22 19:20:20 +0100

    :white_check_mark: Customers can be deleted from the database

--- routes/customers.js
+++ routes/customers.js
@@ -74,4 +74,19 @@ module.exports = server => {
       );
     }
   });
+
+  // Delete customer
+  server.del('/customers/:id', async (req, res, next) => {
+    try {
+      const customer = await Customer.findOneAndRemove({ _id: req.params.id });
+      res.send(204);
+      next();
+    } catch (err) {
+      return next(
+        new errors.ResourceNotFoundError(
+          `There is no customer with the id of ${req.params.id}`
+        )
+      );
+    }
+  });
 };

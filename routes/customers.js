842b9fc8f3dfbb7670d612c9d037e323a30fce42
MarcinBalejko <mbbm9@o2.pl>
2020-02-22 18:23:49 +0100

    :white_check_mark: Creating and adding a customer to the database

--- routes/customers.js
+++ routes/customers.js
@@ -2,6 +2,7 @@ const errors = require('restify-errors');
 const Customer = require('../models/Customer');
 
 module.exports = server => {
+  // Get customers
   server.get('/customers', async (req, res, next) => {
     try {
       const customers = await Customer.find({});
@@ -9,6 +10,32 @@ module.exports = server => {
       next();
     } catch (err) {
       return next(new errors.InvalidContentError(err));
+    }
+  });
+
+  // Add customer
+  server.post('/customers', async (req, res, next) => {
+    // Check for JSON
+    if (!req.is('application/json')) {
+      return next(new errors.InvalidContentError("Expects 'application/json'"));
+    }
+
+    // Creating customer object
+    const { name, email, balance } = req.body;
+
+    const customer = new Customer({
+      name,
+      email,
+      balance
+    });
+
+    // Adding customer to db
+    try {
+      const newCustomer = await customer.save();
+      res.send(201); // 201 = everything is ok and sth was created
+      next();
+    } catch (err) {
+      return next(new errors.InternalError(err.message));
     }
   });
 };
